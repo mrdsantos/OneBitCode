3aa02154d9e7a8877cc6fdf4774cf51b18215a8d
Maicon Rodrigues dos Santos <maiconrdsantos88@gmail.com>
2022-11-18 16:51:47 -0300

    fix some typos, declare variable positions and applicants as an array.

--- Javascript /sistema_de_vagas_de_empregos/content/script/script.js
+++ Javascript /sistema_de_vagas_de_empregos/content/script/script.js
@@ -1,7 +1,9 @@
-let userOption = "0"
+let userOption
+let positions = []
+let applicants = []
 
-const optionPrompt = (userOption) => {
-	UserOption = prompt(
+const optionPrompt = (input) => {
+	userOption = prompt(
 		"Sistema de Vagas de Emprego\n" +
 			"1- Listar vagas disponíveis.\n" +
 			"2- Criar uma nava vaga.\n" +
@@ -10,27 +12,106 @@ const optionPrompt = (userOption) => {
 			"5- Excluir vagas.\n" +
 			"6- Sair."
 	)
+    return userOption
 }
 
+const menu = (userInput) => {
+	while (userOption !== "6" || false) {
+		
+		switch (userOption) {
+			case null:
+				alert("Execução abortada pelo usuário. (Esc)")
+				return false
+			case "6":
+				alert("Programa encerrado pelo usuário")
 
+				break
+			case "1":
+				break
+			case "2":
+				positionNew()
+                optionPrompt()
+				break
+			case "3":
+				break
+			case "4":
+				applicantNew()
+                optionPrompt()
+				break
 
-const menu = () => {
-    while (userOption !== "6" || false) {
-        switch (userOption) {
-            case null:
-                alert("Execução abortada pelo usuário. (Esc)")
-                return false
-            case "6":
-                alert("Programa encerrado pelo usuário")
-                break;
-        
-            default:
-                alert("Por favor selecione uma opção válida")
-                break;
-        }
-
-    }
+			case "5":
+				break
+			default:
+				alert("Por favor selecione uma opção válida")
+				break
+		}
+	}
 }
 
-// userMenu()
-// optionPrompt()
+const positionNew = () => {
+	let posistionListing = {}
+	posistionListing.name = prompt("Digite o nome da vaga:")
+	posistionListing.description = prompt("Digite a descrição da vaga:")
+	posistionListing.duedate = prompt("Digite a data limite:")
+	let confirmation = confirm(
+		"Deseja salvar esta vaga?\n" +
+			posistionListing.name +
+			".\n" +
+			posistionListing.description +
+			".\n" +
+			posistionListing.duedate +
+			".\n"
+	)
+	if (confirmation) {
+		positions.unshift(posistionListing)
+	}
+	return
+}
+
+const applicantNew = () => {
+	let applicantListing = {}
+	applicantListing.name = prompt("Digite o nome do candidato:")
+	applicantListing.email = prompt("Digite o e-mail do candidato:")
+	applicantListing.adress = prompt("Digite o endereço do candidato:")
+	let confirmation = confirm(
+		"Deseja salvar este canidato?\n" +
+			applicantListing.name +
+			".\n" +
+			applicantListing.email +
+			".\n" +
+			applicantListing.adress +
+			"."
+	)
+	if (confirmation) {
+		applicants.unshift(applicantListing)
+	}
+}
+
+//---- Under Construction ↓↓↓
+
+const positionList = (positions) => {
+	let positionAvailable
+	positions.forEach((element) => {})
+
+	alert("Vagas disponíveis:\n")
+}
+
+//---- Standby of functions calls so Quokka doesnt go nuts
+optionPrompt()
+menu()
+
+//---- Templates
+// Position Template
+// {
+//     name: str,
+//     description: str,
+//     duedate: str,
+//     applicants: dynamic
+// }
+
+// Applicant Template
+// {
+//     name: str,
+//     email: str,
+//     address: str,
+// }
