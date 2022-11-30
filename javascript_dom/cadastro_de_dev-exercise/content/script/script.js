0d89657662e5a591ae1c982454d5279336163d5d
Maicon Rodrigues dos Santos <maiconrdsantos88@gmail.com>
2022-11-30 02:23:07 -0300

    break input append into separated functions

--- javascript_dom/cadastro_de_dev-exercise/content/script/script.js
+++ javascript_dom/cadastro_de_dev-exercise/content/script/script.js
@@ -3,7 +3,7 @@
 let developer = {
 	name: "",
 	stack: "",
-	experience: Number,
+	experience: "",
 }
 
 let devRegisterForm = document.getElementById("devRegisterForm")
@@ -14,6 +14,15 @@ devRegisterForm.addEventListener("submit", (ev) => {
 let btnDetails = document.getElementById("btnDevDetails")
 
 btnDetails.addEventListener("click", (ev) => {
+	devRegisterForm.append(devStackInput(), devExperienceInput())
+})
+
+let btnRmvInput = document.createElement("button")
+btnRmvInput.textContent = "-"
+btnRmvInput.className = "btnDetails red"
+
+const devStackInput = () => {
+	let div = document.createElement("div")
 	let labelInputStack = document.createElement("label")
 	labelInputStack.setAttribute("for", "inputDevStack")
 	labelInputStack.textContent = "Tecnologia"
@@ -24,19 +33,65 @@ btnDetails.addEventListener("click", (ev) => {
 	inputStack.type = "text"
 	inputStack.name = "inputDevStack"
 
-	let button = document.createElement("button")
-	button.textContent = "-"
-	button.className = "btnDetails red"
+	let btnRmvInputStack = btnRmvInput.cloneNode(true)
+	btnRmvInputStack.id = "btnRmvInputStack"
+	div.append(labelInputStack, inputStack, btnRmvInputStack)
 
-    let titleRadioExperiece = document.createElement("h4")
-    titleRadioExperiece.textContent = "Experiência"
+	return div
+}
 
-    let radioExperience = document.createElement("radio")
-    radioExperience.id = "radioExperience"
-    radioExperience.type = "radio"
-    radioExperience.name = "radioExperience"
-    
+const devExperienceInput = () => {
+	let div = document.createElement("div")
+
+	let titleDevExperience = document.createElement("h4")
+	titleDevExperience.textContent = "Experiência"
+
+	let devExperienceRadio1 = document.createElement("input")
+	devExperienceRadio1.type = "radio"
+	devExperienceRadio1.id = "devExperienceInput1"
+	devExperienceRadio1.name = "devExperienceInput"
+	devExperienceRadio1.value = "0-2 years"
+
+	let labelDevExperienceRadio1 = document.createElement("label")
+	labelDevExperienceRadio1.setAttribute("for", "devExperienceInput1")
+	labelDevExperienceRadio1.textContent = "0-2 Anos"
+	labelDevExperienceRadio1.className = "checkable inline-block"
+
+	let devExperienceRadio2 = document.createElement("input")
+	devExperienceRadio2.type = "radio"
+	devExperienceRadio2.id = "devExperienceInput2"
+	devExperienceRadio2.name = "devExperienceInput"
+	devExperienceRadio2.value = "3-4 years"
+
+	let labelDevExperienceRadio2 = document.createElement("label")
+	labelDevExperienceRadio2.setAttribute("for", "devExperienceInput2")
+	labelDevExperienceRadio2.textContent = "3-4 Anos"
+	labelDevExperienceRadio2.className = "checkable inline-block"
+
+	let devExperienceRadio3 = document.createElement("input")
+	devExperienceRadio3.type = "radio"
+	devExperienceRadio3.id = "devExperienceInput3"
+	devExperienceRadio3.name = "devExperienceInput"
+	devExperienceRadio3.value = "5+ years"
+
+	let labelDevExperienceRadio3 = document.createElement("label")
+	labelDevExperienceRadio3.setAttribute("for", "devExperienceInput3")
+	labelDevExperienceRadio3.textContent = "5+ Anos"
+	labelDevExperienceRadio3.className = "checkable inline-block"
 
 
-	devRegisterForm.append(labelInputStack, inputStack, button, titleRadioExperiece, radioExperience, button.cloneNode(true))
-})
+    let btnRmvInputExperience = btnRmvInput.cloneNode(true)
+	btnRmvInputExperience.id = "btnRmvInputExperience"
+	div.append(
+		titleDevExperience,
+		devExperienceRadio1,
+		labelDevExperienceRadio1,
+		devExperienceRadio2,
+        labelDevExperienceRadio2,
+        devExperienceRadio3,
+        labelDevExperienceRadio3,
+		btnRmvInputExperience
+	)
+
+	return div
+}
