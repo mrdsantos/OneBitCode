2c576f1982e81bdeebe60bd6ee288f3e2439d109
Maicon Rodrigues dos Santos <maiconrdsantos88@gmail.com>
2022-11-30 22:59:16 -0300

    create new functions to compart and better understand the code
    also tweak css and html for the new functions

--- javascript_dom/cadastro_de_dev-exercise/content/script/script.js
+++ javascript_dom/cadastro_de_dev-exercise/content/script/script.js
@@ -5,99 +5,43 @@ devRegisterForm.addEventListener("submit", (ev) => {
 	ev.preventDefault()
 })
 
-let btnDetails = document.getElementById("btnDevDetails")
-btnDetails.addEventListener("click", (ev) => {
-	btnDetails.disabled = true
-	let formChildren = document.getElementById("formChildren")
-	formChildren.append(devStackInput(), inputDevExperience(), confirmation())
+let devStackBtnAdd = document.getElementById("devStackBtnAdd")
+devStackBtnAdd.addEventListener("click", (ev) => {
+	// devStackBtnAdd.disabled = true
+	let devRegisterForm = document.getElementById("devRegisterForm")
+	devRegisterForm.append(devStacksPlus())
 })
 
-let btnRmvInput = document.createElement("button")
-btnRmvInput.textContent = "-"
-btnRmvInput.className = "btnDetails red"
+let devExperienceRow = 1
 
-const devStackInput = () => {
-	let div = document.createElement("div")
-	let labelInputStack = document.createElement("label")
-	labelInputStack.setAttribute("for", "inputDevStack")
-	labelInputStack.textContent = "Tecnologia"
+const devStacksPlus = () => {
+	let div = createDiv("","")
+    
+    let stackNameInput = createInput("text","devStackInput","Digite a tecnologia conhecida", "devStackTechs", "", "")
+    
+    let devStackBtnAdd = document.getElementById("devStackBtnAdd")
 
-	let inputStack = document.createElement("input")
-	inputStack.id = "inputDevStack"
-	inputStack.placeholder = "Digite a tecnologia conhecida"
-	inputStack.type = "text"
-	inputStack.name = "inputDevStack"
+    let br = document.createElement("br")
+    
+    
+    let rowindex = devExperienceRow
 
-	let btnRmvInputStack = btnRmvInput.cloneNode(true)
-	btnRmvInputStack.id = "btnRmvInputStack"
-	btnRmvInputStack.addEventListener("click", (ev) => {
-		let childElement = document.getElementById("btnRmvInputStack")
-		childElement.parentElement.remove()
-	})
+    let devExperienceLabel0 = createLabel("devExperienceRadio" + rowindex + "-0","0-2 Anos", "checkable inline-block")
+    let devExperienceLabel1 = createLabel("devExperienceRadio" + rowindex + "-1","3-4 Anos", "checkable inline-block")
+    let devExperienceLabel2 = createLabel("devExperienceRadio" + rowindex + "-2","3-4 Anos", "checkable inline-block")
 
-	div.append(labelInputStack, inputStack, btnRmvInputStack)
+    
+    let devExperienceRadio0 = createInput("radio", "devExperience" + rowindex + "-0","","devExperienceRadio", "devExperienceRadio" + rowindex + "-0","0-2 years")
+    let devExperienceRadio1 = createInput("radio", "devExperience" + rowindex + "-1","","devExperienceRadio", "devExperienceRadio" + rowindex + "-1","3-4 years")
+    let devExperienceRadio2 = createInput("radio", "devExperience" + rowindex + "-2","","devExperienceRadio", "devExperienceRadio" + rowindex + "-2","5+ years")
+    
 
-	return div
-}
 
-const inputDevExperience = () => {
-	let div = document.createElement("div")
+    devExperienceRow++
 
-	let titleDevExperience = document.createElement("h4")
-	titleDevExperience.textContent = "Experiência"
+    div.append(stackNameInput, devStackBtnAdd.cloneNode(true), br, devExperienceRadio0, devExperienceLabel0, devExperienceRadio1, devExperienceLabel1, devExperienceRadio2, devExperienceLabel2)
 
-	let devExperienceRadio1 = document.createElement("input")
-	devExperienceRadio1.type = "radio"
-	devExperienceRadio1.id = "inputDevExperience1"
-	devExperienceRadio1.name = "inputDevExperience"
-	devExperienceRadio1.value = "0-2 years"
-
-	let labelDevExperienceRadio1 = document.createElement("label")
-	labelDevExperienceRadio1.setAttribute("for", "inputDevExperience1")
-	labelDevExperienceRadio1.textContent = "0-2 Anos"
-	labelDevExperienceRadio1.className = "checkable inline-block"
-
-	let devExperienceRadio2 = document.createElement("input")
-	devExperienceRadio2.type = "radio"
-	devExperienceRadio2.id = "inputDevExperience2"
-	devExperienceRadio2.name = "inputDevExperience"
-	devExperienceRadio2.value = "3-4 years"
-
-	let labelDevExperienceRadio2 = document.createElement("label")
-	labelDevExperienceRadio2.setAttribute("for", "inputDevExperience2")
-	labelDevExperienceRadio2.textContent = "3-4 Anos"
-	labelDevExperienceRadio2.className = "checkable inline-block"
-
-	let devExperienceRadio3 = document.createElement("input")
-	devExperienceRadio3.type = "radio"
-	devExperienceRadio3.id = "inputDevExperience3"
-	devExperienceRadio3.name = "inputDevExperience"
-	devExperienceRadio3.value = "5+ years"
-
-	let labelDevExperienceRadio3 = document.createElement("label")
-	labelDevExperienceRadio3.setAttribute("for", "inputDevExperience3")
-	labelDevExperienceRadio3.textContent = "5+ Anos"
-	labelDevExperienceRadio3.className = "checkable inline-block"
-
-	let btnRmvInputExperience = btnRmvInput.cloneNode(true)
-	btnRmvInputExperience.id = "btnRmvInputExperience"
-	btnRmvInputExperience.addEventListener("click", (ev) => {
-		let childElement = document.getElementById("btnRmvInputExperience")
-		childElement.parentElement.remove()
-	})
-
-	div.append(
-		titleDevExperience,
-		devExperienceRadio1,
-		labelDevExperienceRadio1,
-		devExperienceRadio2,
-		labelDevExperienceRadio2,
-		devExperienceRadio3,
-		labelDevExperienceRadio3,
-		btnRmvInputExperience
-	)
-
-	return div
+    return div
 }
 
 const confirmation = () => {
@@ -110,7 +54,7 @@ const confirmation = () => {
 		saveData()
 		btnDetails.disabled = false
 		resetInput()
-		let formChildren = document.getElementById("formChildren")
+		let formChildren = document.getElementById("formCh ildren")
 		while (formChildren.firstChild) {
 			formChildren.removeChild(formChildren.firstChild)
 		}
@@ -151,6 +95,35 @@ const saveData = () => {
 	).value
 
 	developer.push(newDeveloper)
-
-	return
 }
+
+const createLabel = (forName, textContent, className) => {
+	let element = document.createElement("label")
+	element.htmlFor = forName
+	element.textContent = textContent
+	element.className = className
+
+	return element
+}
+
+const createInput = (type, name, placeholder, className, id, value) => {
+	let element = document.createElement("input")
+	element.type = type
+	element.name = name
+	element.placeholder = placeholder
+	element.className = className
+	element.id = id
+	element.value = value
+
+	return element
+}
+
+const createDiv = () => {
+	let element = document.createElement("div")
+
+	return element
+}
+
+let btnRmvInput = document.createElement("button")
+btnRmvInput.textContent = "-"
+btnRmvInput.className = "btnDetails red"
