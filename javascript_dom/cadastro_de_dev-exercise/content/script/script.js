d86daf08d6064386822f5ba16ebe552c1ea617e8
Maicon Rodrigues dos Santos <maiconrdsantos88@gmail.com>
2022-12-05 23:13:29 -0300

    major fix in all script.css
    fixes in most functions - cuz they were wrong you know
    fix in declaration order - cuz they were also wrong can u believe?
    minor tweaks to html and css

--- javascript_dom/cadastro_de_dev-exercise/content/script/script.js
+++ javascript_dom/cadastro_de_dev-exercise/content/script/script.js
@@ -5,141 +5,11 @@ devRegisterForm.addEventListener("submit", (ev) => {
 	ev.preventDefault()
 })
 
-let devStackBtnAdd = document.getElementById("devStackBtnAdd")
-devStackBtnAdd.addEventListener("click", (ev) => {
-	let formChildren = document.getElementById("formChildren")
-	formChildren.append(devStacksPlus())
-})
+let devExperienceRow = 0
 
-let devExperienceRow = 1
+let formChildren = document.getElementById("formChildren")
 
-const devStacksPlus = () => {
-	let div = createDiv("", "")
-
-	let stackNameInput = createInput(
-		"text",
-		"devStackInput",
-		"Digite a tecnologia conhecida",
-		"devStackTechs",
-		"",
-		""
-	)
-
-	let br = document.createElement("br")
-
-	let rowindex = devExperienceRow
-
-	let devExperienceLabel0 = createLabel(
-		"devExperienceRadio" + rowindex + "-0",
-		"0-2 Anos",
-		"checkable inline-block"
-	)
-	let devExperienceLabel1 = createLabel(
-		"devExperienceRadio" + rowindex + "-1",
-		"3-4 Anos",
-		"checkable inline-block"
-	)
-	let devExperienceLabel2 = createLabel(
-		"devExperienceRadio" + rowindex + "-2",
-		"3-4 Anos",
-		"checkable inline-block"
-	)
-
-	let devExperienceRadio0 = createInput(
-		"radio",
-		"devExperience" + rowindex + "-0",
-		"",
-		"devExperienceRadio",
-		"devExperienceRadio" + rowindex + "-0",
-		"0-2 years"
-	)
-	let devExperienceRadio1 = createInput(
-		"radio",
-		"devExperience" + rowindex + "-1",
-		"",
-		"devExperienceRadio",
-		"devExperienceRadio" + rowindex + "-1",
-		"3-4 years"
-	)
-	let devExperienceRadio2 = createInput(
-		"radio",
-		"devExperience" + rowindex + "-2",
-		"",
-		"devExperienceRadio",
-		"devExperienceRadio" + rowindex + "-2",
-		"5+ years"
-	)
-
-	let devStackBtnRmv = btnRmvInput()
-
-	devExperienceRow++
-
-	div.append(
-		stackNameInput,
-		devStackBtnRmv,
-		br,
-		devExperienceRadio0,
-		devExperienceLabel0,
-		devExperienceRadio1,
-		devExperienceLabel1,
-		devExperienceRadio2,
-		devExperienceLabel2
-	)
-
-	return div
-}
-
-const confirmation = () => {
-	let div = createDiv()
-	let btnSave = document.createElement("button")
-	btnSave.textContent = "Salvar"
-	btnSave.id = "btnSave"
-	btnSave.className = "green"
-	btnSave.addEventListener("click", (ev) => {
-		saveData()
-		resetInput()
-		let formChildren = document.getElementById("formChildren")
-		while (formChildren.firstChild) {
-			formChildren.removeChild(formChildren.firstChild)
-		}
-	})
-
-	let btnCancel = document.createElement("button")
-	btnCancel.textContent = "Cancelar"
-	btnCancel.id = "btnCancel"
-	btnCancel.className = "red"
-	btnCancel.addEventListener("click", (ev) => {
-		resetInput()
-		let formChildren = document.getElementById("formChildren")
-		while (formChildren.firstChild) {
-			formChildren.removeChild(formChildren.firstChild)
-		}
-	})
-
-	div.append(btnCancel, btnSave)
-	return div
-}
-
-const resetInput = () => {
-	let resetInput = document.getElementById("devNameInput")
-	resetInput.value = ""
-}
-
-const saveData = () => {
-	let newDeveloper = {}
-	newDeveloper.name = document.querySelector(
-		`input[name="devNameInput"]`
-	).value
-	newDeveloper.stack = document.querySelector(
-		`input[name="devStackInput"]`
-	).value
-	newDeveloper.experience = document.querySelector(
-		`input[class="devExperienceRadio"]:checked`
-	).value
-
-	developer.push(newDeveloper)
-}
-
+// function templates for creating elements
 const createLabel = (forName, textContent, className) => {
 	let element = document.createElement("label")
 	element.htmlFor = forName
@@ -161,29 +31,190 @@ const createInput = (type, name, placeholder, className, id, value) => {
 	return element
 }
 
-const createDiv = () => {
+const createDiv = (id, className) => {
 	let element = document.createElement("div")
+	element.id = id
+	element.className = className
 
 	return element
 }
 
-const btnRmvInput = () => {
+const createBtnRmvTech = (id, className) => {
 	let button = document.createElement("button")
-	let rowindex = devExperienceRow
-
 	button.textContent = "-"
-	button.className = "btnDetails red"
-	button.id = "devStackBtnRmv-" + rowindex
+	button.className = className
+	button.id = id
 	button.addEventListener("click", (ev) => {
 		let elementId = document.getElementById(button.id)
 		elementId.parentElement.remove()
 	})
-	devExperienceRow++
-
 	return button
 }
 
-//append confirmation after the Developer Techs
+const createBtnAddTech = (id, className) => {
+	let button = document.createElement("button")
+	button.textContent = "+"
+	button.id = id
+	button.className = className
+	button.type = "button"
+	button.addEventListener("click", (ev) => {
+		let element = document.getElementById("formChildren")
+		element.append(devTechAdd())
+	
+	})
+	return button
+}
+
+// append elements functions
+
+//devTechAdd receive a conditional paramenter. If the parameter is == 1
+//doenst append btnRmvTech and append the Technologies Title Element
+//ued to append the first tech input without appending that said button
+
+const devTechAdd = (conditional) => {
+	let rowIndex = devExperienceRow
+	let div = createDiv("devTechRow" + rowIndex, "devTech")
+	let techTitle = document.createElement("h3")
+	techTitle.textContent = "Tecnologias"
+
+	let techNameInput = createInput(
+		"text",
+		"devTechInput",
+		"Digite a tecnologia conhecida",
+		"",
+		"devTechInput" + rowIndex,
+		""
+	)
+
+	let br = document.createElement("br")
+
+	let devExperienceLabel0 = createLabel(
+		"devExperienceRadio" + rowIndex + "-0",
+		"0-2 Anos",
+		"checkable inline-block"
+	)
+	let devExperienceLabel1 = createLabel(
+		"devExperienceRadio" + rowIndex + "-1",
+		"3-4 Anos",
+		"checkable inline-block"
+	)
+	let devExperienceLabel2 = createLabel(
+		"devExperienceRadio" + rowIndex + "-2",
+		"5+ Anos",
+		"checkable inline-block"
+	)
+
+	let devExperienceRadio0 = createInput(
+		"radio",
+		"devExperience" + rowIndex,
+		"",
+		"devExperienceRadio ",
+		"devExperienceRadio" + rowIndex + "-0",
+		"0-2 years"
+	)
+	let devExperienceRadio1 = createInput(
+		"radio",
+		"devExperience" + rowIndex,
+		"",
+		"devExperienceRadio ",
+		"devExperienceRadio" + rowIndex + "-1",
+		"3-4 years"
+	)
+	let devExperienceRadio2 = createInput(
+		"radio",
+		"devExperience" + rowIndex,
+		"",
+		"devExperienceRadio ",
+		"devExperienceRadio" + rowIndex + "-2",
+		"5+ years"
+	)
+
+	devExperienceRow++
+
+	if (conditional == 1) {
+		div.append(
+			techTitle,
+			techNameInput,
+			createBtnAddTech("btnAddrow" + rowIndex, "btnTech"),
+			br,
+			devExperienceRadio0,
+			devExperienceLabel0,
+			devExperienceRadio1,
+			devExperienceLabel1,
+			devExperienceRadio2,
+			devExperienceLabel2
+		)
+		return div
+	}
+	div.append(
+			techNameInput,
+			createBtnAddTech("btnAddrow" + rowIndex, "btnTech"),
+			createBtnRmvTech("btnRmvRow" + rowIndex, "btnTech red"),
+			br,
+			devExperienceRadio0,
+			devExperienceLabel0,
+			devExperienceRadio1,
+			devExperienceLabel1,
+			devExperienceRadio2,
+			devExperienceLabel2
+		)
+
+	return div
+}
+
+// dom manipulation functions
+formChildren.append(devTechAdd(1))
+
+const confirmation = () => {
+	let div = createDiv()
+	let btnSave = document.createElement("button")
+	btnSave.textContent = "Salvar"
+	btnSave.id = "btnSave"
+	btnSave.className = "green"
+	btnSave.addEventListener("click", (ev) => {
+		saveData()
+		resetInput()
+		btnSave.blur()
+	})
+
+	let btnCancel = document.createElement("button")
+	btnCancel.textContent = "Cancelar"
+	btnCancel.id = "btnCancel"
+	btnCancel.className = "red"
+	btnCancel.addEventListener("click", (ev) => {
+		resetInput()
+		btnCancel.blur()		
+	})
+
+	div.append(btnCancel, btnSave)
+	return div
+}
+
+const resetInput = () => {
+	let element = document.getElementById("formChildren")
+	while (element.hasChildNodes()) {
+		element.lastChild.remove()
+	}
+	formChildren.append(devTechAdd(1))
+}
+
+const saveData = () => {
+	let fullName = document.getElementById("devNameInput").value
+
+	let technologies = []
+	let tech = document.querySelectorAll(".devTech")
+	tech.forEach((element ) => {
+		let techName = document.querySelector("#" + element.id + " input[name='devTechInput']").value
+		let techExp = 	document.querySelector("#" + element.id + " input[type='radio']:checked").value																
+		technologies.push({technologies: techName, Exp: techExp})
+	})
+
+	let newDeveloper = {Name: fullName, Technologies: technologies}
+
+	developer.push(newDeveloper)
+}
+
+//append confirmation() after the Developer Techs
 {
 	let elementConfirmation = document.getElementById("devRegisterForm")
 	elementConfirmation.append(confirmation())
